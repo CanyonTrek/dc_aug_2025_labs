c6c5eb4f326507b6554602facb8f51e3fbe93ca4
Donald Cameron <donald.cameron@qa.com>
2025-08-06 16:38:40 +0100

    Create Test class and 1st test - find highest in a list of one result in one value

---
+++ highest_number_services/find_highest_number/tests/test_highest_number_finder.py
@@ -0,0 +1,17 @@
+import unittest
+
+class TestHighestNumberFinder(unittest.TestCase):
+    def test_find_highest_in_list_of_one_expect_single_item(self):
+        # Arrange
+        numbers = [10]
+        expected_result = 10
+        cut = HighestNumberFinder() # Class Under Test
+
+        # Act
+        actual_result = cut.find_highest_number(numbers)
+
+        # Assert
+        self.assertEqual(expected_result, actual_result)
+
+if __name__ == "__main__":
+    unittest.main()
