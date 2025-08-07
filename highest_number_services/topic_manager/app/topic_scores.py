b9261e9f6673c071d162115144fe98da9077578e
Donald Cameron <donald.cameron@qa.com>
2025-08-07 11:34:46 +0100

    Updated topic_scores with getter methods for topic_name and scores

--- highest_number_services/topic_manager/app/topic_scores.py
+++ highest_number_services/topic_manager/app/topic_scores.py
@@ -1,2 +1,10 @@
 class TopicScores:
-    pass
+    def __init__(self, topic_name, scores):
+        self._topic_name = topic_name
+        self._scores = scores
+
+    def get_topic_name(self):
+        return self._topic_name
+
+    def get_score(self):
+        return self._scores
