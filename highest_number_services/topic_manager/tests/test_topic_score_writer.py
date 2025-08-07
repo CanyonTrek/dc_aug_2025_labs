dc143317391abab7310b9f0c76e65a30f2ab8034
Donald Cameron <donald.cameron@qa.com>
2025-08-07 16:11:23 +0100

    Added 3rd test

--- highest_number_services/topic_manager/tests/test_topic_score_writer.py
+++ highest_number_services/topic_manager/tests/test_topic_score_writer.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock, call
 
 from topic_top_score import TopicTopScore
 from topic_score_writer import TopicScoreWriter
@@ -33,6 +33,39 @@ class TestTopicScoreWriter(unittest.TestCase):
 
         # Assert
         mock_file_writer.write_line.assert_not_called()
+
+    def test_verify_topic_score_details_written_out_multiple_times(self):
+        # Arrange
+        physics = "Physics"
+        art = "Art"
+        comp_sci = "Comp Sci"
+
+        physics_result = "Physics, 89"
+        art_result = "Art, 87"
+        comp_sci_result = "Comp Sci, 97"
+
+        top_scores = [
+            TopicTopScore(physics, 89),
+            TopicTopScore(art, 87),
+            TopicTopScore(comp_sci, 97),
+        ]
+
+        mock_file_writer = Mock()
+        cut = TopicScoreWriter(mock_file_writer)
+
+        # Act
+        cut.write_scores(top_scores)
+
+        # Assert
+        expected_calls = [
+            call.write_line(physics_result),
+            call.write_line(art_result),
+            call.write_line(comp_sci_result)
+        ]
+        mock_file_writer.write_line.assert_has_calls(expected_calls, any_order=False)
+        self.assertEqual(mock_file_writer.write_line.call_count, 3)
+
+
 
 if __name__ == "__main__":
     unittest.main()
