cfcb29341646c0257594721a8932dfe42d5d399e
Donald Cameron <donald.cameron@qa.com>
2025-08-07 11:38:32 +0100

    Updated topic_top_score with getter methods for topic_name and top_scores

--- highest_number_services/topic_manager/app/topic_top_score.py
+++ highest_number_services/topic_manager/app/topic_top_score.py
@@ -1,2 +1,10 @@
 class TopicTopScore:
-    pass
+    def __init__(self, topic_name, score):
+        self._topic_name = topic_name
+        self._top_score = score
+
+    def get_topic_name(self):
+        return self._topic_name
+
+    def get_top_score(self):
+        return self._top_score
