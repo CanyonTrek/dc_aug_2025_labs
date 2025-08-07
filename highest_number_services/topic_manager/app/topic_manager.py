d5ecd51cf37c0d2042a4845d130e93b76f36b5b6
Donald Cameron <donald.cameron@qa.com>
2025-08-07 11:57:46 +0100

    Updated topic_manager with to pass 2nd test for highest score with list of one topic+scores return list of one

--- highest_number_services/topic_manager/app/topic_manager.py
+++ highest_number_services/topic_manager/app/topic_manager.py
@@ -1,4 +1,21 @@
+import os
+import sys
+
+mpath = os.path.abspath(os.path.join(os.path.dirname(__file__), "..","..", "find_highest_number"))
+sys.path.insert(0, mpath)
+
+from app.highest_number_finder import HighestNumberFinder
+from topic_top_score import TopicTopScore
+
 class TopicManager:
-    def find_topic_high_scores(self, topic_score_list):
+    def __init__(self):
+        self._highest_number_finder = HighestNumberFinder()
+
+    def find_topic_high_scores(self, topic_scores_list):
         top_scores = []
+
+        if len(topic_scores_list) == 1:
+            ts = topic_scores_list[0]
+            top_score = self._highest_number_finder.find_highest_number(ts.get_scores())
+            top_scores.append(TopicTopScore(ts.get_topic_name(), top_score))
         return top_scores
