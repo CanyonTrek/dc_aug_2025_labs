d468a123d79f410b348ce8374b62f54608aa387e
Donald Cameron <donald.cameron@qa.com>
2025-08-07 11:49:06 +0100

    Updated test_topic_top_score with 2nd test for highest score with list of one topic+scores return list of one

--- highest_number_services/topic_manager/tests/test_topic_manager.py
+++ highest_number_services/topic_manager/tests/test_topic_manager.py
@@ -1,4 +1,8 @@
 import unittest
+from topic_manager.app.topic_manager import TopicManager
+from topic_manager.app.topic_scores import TopicScores
+from topic_manager.app.topic_top_score import TopicTopScore
+
 
 class TestTopicManager(unittest.TestCase):
     def test_find_highest_score_in_empty_list_returns_empty_list(self):
@@ -12,6 +16,21 @@ class TestTopicManager(unittest.TestCase):
 
         # Assert
         self.assertEqual(expected_result, actual_result)
+
+    def test_find_highest_score_with_list_of_one_returns_list_of_one(self):
+        # Arrange
+        scores = [56, 67, 43, 89]
+        topic_name = "Physics"
+        topic_scores = [TopicScores(topic_name, scores)]
+        expected_result = [TopicTopScore(topic_name, 89)]
+        cut = TopicManager()
+
+        # Act
+        actual_result = cut.find_topic_high_scores(topic_scores)
+
+        # Assert
+        self.assertEqual(expected_result[0].get_topic_name(), actual_result[0].get_topic_name())
+        self.assertEqual(expected_result[0].get_top_score(), actual_result[0].get_top_score())
 
 if __name__ == "__main__":
     unittest.main()
